fed3f557016bdb86cde42bb82ce0f8e395481c5e
Aaron Parker <aaron@stealthpuppy.com>
2023-09-24 10:18:31 +1000

    0.2.0
    Add Test-VMwareProductDownload, update existing functions, return more download file details

--- src/Evergreen.VMware.psd1
+++ src/Evergreen.VMware.psd1
@@ -8,10 +8,10 @@
 
 @{
     # Script module or binary module file associated with this manifest.
-    # RootModule = ''
+    RootModule            = 'Evergreen.VMware.psm1'
 
     # Version number of this module.
-    ModuleVersion         = '0.1.0'
+    ModuleVersion         = '0.2.0'
 
     # Supported PSEditions
     # CompatiblePSEditions = @()
@@ -32,7 +32,7 @@
     Description           = 'A PowerShell module for retrieving available downloads from vmware.com'
 
     # Minimum version of the PowerShell engine required by this module
-    PowerShellVersion     = '5.0.0'
+    PowerShellVersion     = '5.0'
 
     # Name of the PowerShell host required by this module
     # PowerShellHostName = ''
@@ -68,7 +68,7 @@
     # NestedModules = @()
 
     # Functions to export from this module, for best performance, do not use wildcards and do not delete the entry, use an empty array if there are no functions to export.
-    FunctionsToExport     = @("Get-VMwareAPIPath", "Get-VMwareProductList", "Get-VMwareRelatedDLGList", "Get-VMwareDLGHeaderQuery", "Get-VMwareDLGDetailsQuery", "Get-VMwareProductDownload")
+    FunctionsToExport     = @("Get-VMwareProductList", "Get-VMwareRelatedDLGList", "Get-VMwareProductDownload", "Test-VMwareProductDownload")
 
     # Cmdlets to export from this module, for best performance, do not use wildcards and do not delete the entry, use an empty array if there are no cmdlets to export.
     CmdletsToExport       = @()
@@ -90,10 +90,14 @@
 
     # Private data to pass to the module specified in RootModule/ModuleToProcess. This may also contain a PSData hashtable with additional module metadata used by PowerShell.
     PrivateData           = @{
-        PSData = @{
+
+        #RepositorySourceLocation of this module
+        RepositorySourceLocation = 'https://github.com/EUCPilots/evergreen.vmware/'
+
+        PSData                   = @{
 
             # Tags applied to this module. These help with module discovery in online galleries.
-            Tags       = @('vmware')
+            Tags       = @('vmware', 'evergreen')
 
             # A URL to the license for this module.
             LicenseUri = 'https://github.com/EUCPilots/evergreen.vmware/blob/main/LICENSE'
--- src/Evergreen.VMware.psm1
+++ src/Evergreen.VMware.psm1
@@ -1,9 +1,16 @@
 <#
     .SYNOPSIS
         Script to initiate the module
+
+    .NOTES
+        Original code: https://github.com/aaronparker/evergreen/issues/474#issuecomment-1492766858
 #>
 [CmdletBinding(SupportsShouldProcess = $false)]
 param ()
+
+# Enable TLS 1.2
+[System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072
+$ProgressPreference = [System.Management.Automation.ActionPreference]::SilentlyContinue
 
 function Get-VMwareAPIPath {
     [CmdletBinding(SupportsShouldProcess = $false)]
@@ -55,7 +62,7 @@ function Get-VMwareRelatedDLGList {
         UseBasicParsing = $true
         ErrorAction     = "Stop"
     }
-    $WebResult = Invoke-WebRequest @params | ConvertFrom-Json -ErrorAction "Stop"
+    $WebResult = Invoke-RestMethod @params
     Write-Output -InputObject $WebResult.dlgEditionsLists.dlgList
 }
 
@@ -89,7 +96,7 @@ function Get-VMwareDLGDetailsQuery {
     param (
         [Parameter(Mandatory = $true)]
         [System.String] $DownloadGroup,
-    
+
         [Parameter(Mandatory = $false)]
         [System.String] $Locale = 'en_US'
     )
@@ -116,8 +123,8 @@ function Get-VMwareProductList {
         UseBasicParsing = $true
         ErrorAction     = "Stop"
     }
-    $WebResult = Invoke-WebRequest @params | ConvertFrom-Json -ErrorAction "Stop"
-    
+    $WebResult = Invoke-RestMethod @params
+
     $FilteredProductList = $WebResult.productCategoryList.ProductList
     if ($PSBoundParameters.ContainsKey('Name')) {
         $FilteredProductList = $FilteredProductList | Where-Object -FilterScript { $_.Name -eq $Name }
@@ -151,24 +158,112 @@ function Get-VMwareProductDownload {
 
             foreach ($Dlg in $VMwareDLG) {
                 $params = @{
-                    Uri         = $(Get-VMwareDLGDetailsQuery -downloadGroup $Dlg.code)
+                    Uri         = $(Get-VMwareDLGDetailsQuery -DownloadGroup $Dlg.code)
                     ErrorAction = "SilentlyContinue"
                 }
                 $DownloadFiles = $(Invoke-RestMethod @params).downloadFiles
+
                 foreach ($File in $DownloadFiles) {
-                    $Result = [PSCustomObject]@{
-                        ProductName = $Product
-                        Download    = $File.title
-                        Category    = $VMwareProduct.CategoryMap
-                        Version     = $File.version
-                        URI         = "https://download3.vmware.com/software/$($Dlg.code)/$($File.fileName)"
+                    if ([System.String]::IsNullOrEmpty($File.title)) {
                     }
-                    Write-Output -InputObject $Result
+                    else {
+                        $Result = [PSCustomObject]@{
+                            ProductName = $Product
+                            Download    = $File.title
+                            Category    = $VMwareProduct.CategoryMap
+                            Md5         = $File.md5checksum
+                            ReleaseDate = $File.releaseDate
+                            Version     = $File.version
+                            URI         = "https://download3.vmware.com/software/$($Dlg.code)/$($File.fileName)"
+                        }
+                        Write-Output -InputObject $Result
+                    }
                 }
             }
         }
     }
 }
 
+function Test-VMwareProductDownload {
+    <#
+        .EXTERNALHELP Evergreen-help.xml
+    #>
+    [OutputType([System.Management.Automation.PSObject])]
+    [CmdletBinding(SupportsShouldProcess = $true, HelpURI = "https://stealthpuppy.com/evergreen/test/", DefaultParameterSetName = "Path")]
+    param (
+        [Parameter(
+            Mandatory = $true,
+            Position = 0,
+            ValueFromPipeline)]
+        [ValidateNotNull()]
+        [System.Management.Automation.PSObject] $InputObject,
+
+        [Parameter(Mandatory = $false, Position = 1)]
+        [System.String] $Proxy,
+
+        [Parameter(Mandatory = $false, Position = 2)]
+        [System.Management.Automation.PSCredential]
+        $ProxyCredential = [System.Management.Automation.PSCredential]::Empty,
+
+        [Parameter()]
+        [ValidateNotNullOrEmpty()]
+        [System.String] $UserAgent = [Microsoft.PowerShell.Commands.PSUserAgent]::Chrome,
+
+        [Parameter(Mandatory = $false)]
+        [System.Management.Automation.SwitchParameter] $Force
+    )
+
+    process {
+        # Loop through each object and download to the target path
+        foreach ($Object in $InputObject) {
+
+            #region Validate the URI property and find the output filename
+            if ([System.Boolean]($Object.URI)) {
+            }
+            else {
+                throw "Object does not have valid URI property."
+            }
+            #endregion
+
+            try {
+                $params = @{
+                    Uri             = $Object.URI
+                    Method          = "HEAD"
+                    UseBasicParsing = $true
+                    UserAgent       = $UserAgent
+                    ErrorAction     = "SilentlyContinue"
+                }
+                if ($PSBoundParameters.ContainsKey("Proxy")) {
+                    $params.Proxy = $Proxy
+                }
+                if ($PSBoundParameters.ContainsKey("ProxyCredential")) {
+                    $params.ProxyCredential = $ProxyCredential
+                }
+                $Result = $true
+                Invoke-WebRequest @params | Out-Null
+            }
+            catch [System.Exception] {
+                $Result = $false
+            }
+
+            $PSObject = [PSCustomObject] @{
+                Result   = $Result
+                Product  = $Object.ProductName
+                Category = $Object.Category
+                Version  = $Object.Version
+                URI      = $Object.URI
+            }
+            Write-Output -InputObject $PSObject
+        }
+    }
+
+    end {
+        if ($PSCmdlet.ShouldProcess("Remove variables")) {
+            if (Test-Path -Path Variable:params) { Remove-Variable -Name "params" -ErrorAction "SilentlyContinue" }
+            Remove-Variable -Name "OutPath", "OutFile" -ErrorAction "SilentlyContinue"
+        }
+    }
+}
+
 # Export the public modules and aliases
-Export-ModuleMember -Function "Get-VMwareProductList", "Get-VMwareRelatedDLGList", "Get-VMwareProductDownload" -Alias *
+Export-ModuleMember -Function "Get-VMwareProductList", "Get-VMwareRelatedDLGList", "Get-VMwareProductDownload", "Test-VMwareProductDownload" -Alias *
